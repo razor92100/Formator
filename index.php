73648b446c74ccc3f445ac529c81b7a6ed0fcf0c
Julien BONNETAUD <julien.bonnetaud@gmail.com>
2014-11-26 11:01:55 +0100

    StringFormator update

--- index.php
+++ index.php
@@ -8,5 +8,7 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-$string = "If length is given and is negative, then that many characters will be omitted from the end of string (after the start position has been calculated when a start is negative). If start denotes the position of this truncation or beyond, false will be returned.";
-echo \JB\Formator\StringFormator::getResume($string);
+$filterWords = array('caca', 'pute', 'salope', 'merde');
+
+$string = "Ta mere la pute salope de merde";
+echo \JB\Formator\StringFormator::noBadWords($string, $filterWords);
--- src/StringFormator.php
+++ src/StringFormator.php
@@ -8,14 +8,74 @@
 
 namespace JB\Formator;
 
+/**
+ * Class StringFormator
+ * @package JB\Formator
+ */
 class StringFormator
 {
+    /**
+     *
+     */
     const DEFAULT_LENGTH = 120;
 
+    /**
+     * @param string $string
+     * @param int $length
+     *
+     * @return string
+     *
+     * @throws \Exception
+     */
     public static function getResume($string, $length = self::DEFAULT_LENGTH)
     {
-        $resume = substr($string, 0, $length);
-        return $resume;
-        echo $resume;
+        if (false === is_string($string)) {
+            throw new \Exception('$string must be a string');
+        }else {
+            $resume = substr($string, 0, $length);
+            return $resume;
+        }
+    }
+
+    public static function checkForChar($string, $char)
+    {
+        if (is_string($string) || is_string($char)) {
+            if (strpos($string, $char)) {
+                return true;
+            }else {
+                return false;
+            }
+        }else {
+            throw new \Exception('$string and $char must be a string');
+        }
+    }
+    public static function noBadWords($string, $arrayWords)
+    {
+        if (is_string($string) || is_array($arrayWords)) {
+            $res = str_replace($arrayWords, '****', $string);
+            return $res;
+        }else {
+            throw new \Exception('$string must a string and $arrayWords must be an array');
+        }
+    }
+
+    /**
+     * @param $string
+     * @param $maxLength
+     *
+     * @return string
+     *
+     * @throws \Exception
+     */
+    public static function cleanString($string, $maxLength = self::DEFAULT_LENGTH)
+    {
+        if (false === is_string($string) || false === is_int($maxLength)) {
+            throw new \Exception('$string must be a string and $maxLength an integer');
+        }else {
+            $cleanString = trim($string);
+            $cleanString = self::getResume($cleanString);
+
+            return $cleanString;
+        }
     }
 } 
